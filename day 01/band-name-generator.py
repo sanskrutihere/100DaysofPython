3988031525a8c905423def3f60f85011c7ed16d9
Sansuu <111652527+sanskrutihere@users.noreply.github.com>
2024-10-01 16:32:34 +0530

    Update band-name-generator.py

--- day 01/band-name-generator.py
+++ day 01/band-name-generator.py
@@ -1,3 +1,7 @@
+'''
+Greet user,ask user for city they grew up in,name of pet and combine city and pet name to get band name
+'''
+
 print("Welcome to Band Name Generator! :)")
 city = input("What's the name of the city you grew up in?\n")
 pet = input("What's your pet's name?\n")
